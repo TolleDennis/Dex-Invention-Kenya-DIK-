d0d8d507ae3487f02243802242cd84a7e42eddce
TolleDennis <tolledennis89@gmail.com>
2023-09-15 09:04:42 +0300

    components

---
+++ smart contract/PictureOwnership.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+contract PictureOwnership {
+    mapping(uint256 => address) public pictureToOwner;
+
+    function transferOwnership(uint256 pictureId, address newOwner) public {
+        require(msg.sender == pictureToOwner[pictureId], "Only the owner can transfer ownership.");
+        pictureToOwner[pictureId] = newOwner;
+    }
+}
---
+++ src/components/upload.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function ImageUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    // Implement file upload logic here, e.g., use a backend server.
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+}
+
+export default ImageUpload;
